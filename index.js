1ab6caf5f99e3b0e90706a6656ea120cf5149d7d
AlexisArias874 <pena.arias.josue.alexis@gmail.com>
2026-03-04 18:10:16 -0600

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.post('/webhook', async (req, res) => {
     try {
         // Usamos la versión 2.0 que apareció en tu lista de modelos disponibles
         const model = genAI.getGenerativeModel({ 
-            model: "gemini-2.0-flash" 
+            model: "gemini-2.5-flash-preview-tts" 
         });
 
         const chat = model.startChat({
@@ -41,3 +41,4 @@ app.post('/webhook', async (req, res) => {
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Servidor listo en puerto ${PORT}`));
+
